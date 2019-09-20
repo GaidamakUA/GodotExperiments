474600f627c1d677eb05e90f259fd3cb296b9eaa
Yurii Mazurevych <jmazurevych@gmail.com>
2019-09-20 23:57:18 +0300

    Refactoring and main menu

---
+++ arcanoid/scenes/MainMenu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://scenes/MainScreen.tscn")
--- arcanoid/scenes/MainScreen.gd
+++ arcanoid/scenes/MainScreen.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _on_Area2D_body_entered(body):
-	print("You lost")
 	get_parent().remove_child(body)
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
